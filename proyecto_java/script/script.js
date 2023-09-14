c228218e184ae8b7685752046438b06a0ee47716
Cesarsol117 <cesar_p117@hotmail.com>
2023-09-14 09:07:00 -0500

    clase_20: menu hamburguesa con toogle y media query

--- proyecto_java/script/script.js
+++ proyecto_java/script/script.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email'); 
 const desktopMenu = document.querySelector('.desktop-menu'); 
+// menu hambueguesa
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu  = document.querySelector('.mobile-menu');
 // 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+menuHamIcon.addEventListener('click', toogleHamMenu);
 
 function toogleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+// `para ver el menu hamburguesa
+function toogleHamMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
