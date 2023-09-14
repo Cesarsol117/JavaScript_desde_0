d73eed5a7053cdbaa7747b560d7ebeb04b11cb93
Cesarsol117 <cesar_p117@hotmail.com>
2023-09-14 10:07:41 -0500

    clase_21: carrito de compras y toogle y add para cuando demos click otro menu se oculte

--- proyecto_java/script/script.js
+++ proyecto_java/script/script.js
@@ -4,13 +4,27 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu  = document.querySelector('.mobile-menu');
 // 
+// aside
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const menuAside = document.querySelector('.product-detail');
+// 
+
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuHamIcon.addEventListener('click', toogleHamMenu);
+menuCarrito.addEventListener('click', toogleCarritoAside);
 
 function toogleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
+    menuAside.classList.add('inactive');
 }
 // `para ver el menu hamburguesa
 function toogleHamMenu(){
-    mobileMenu.classList.toggle('inactive')
+    mobileMenu.classList.toggle('inactive');
+    menuAside.classList.add('inactive');
+}
+// para el carrito de compras
+function toogleCarritoAside(){
+    menuAside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive')
 }
